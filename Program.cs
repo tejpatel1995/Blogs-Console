727212d16538375cfd6cfe6997dff68cc4881dc0
Tej Patel <tpatel2@my.wctc.edu>
2020-04-23 10:35:22 -0500

    Final Submit

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace BlogsConsole
                 do
                 {
 
-                    Console.WriteLine("Select action: \n1. Display Blogs\n2. Add Blog\n3. Create Post\n4. Display Posts\nPress any other key to Quit");
+                    Console.WriteLine("Select action: \n1. Display Blogs\n2. Add Blog\n3. Create Post\nPress any other key to Quit");
                     answer = Console.ReadLine();
                     Console.Clear();
                     if (answer == "1")
@@ -99,53 +99,8 @@ namespace BlogsConsole
                             logger.Error("Blog is Nan");
                         }
                     }
-                    if (answer == "4")
-                    {
-
-                        var db = new BloggingContext();
-                        var query = db.Blogs.OrderBy(b => b.BlogId);
-                        Console.WriteLine("Which blogs posts would you like to view?\n0. Posts from all blogs");
-                        int tempMax = 0;                       
-                        foreach (var item in query)
-                        {
-                            Console.WriteLine($"{item.BlogId}. Posts from blog: {item.Name}");
-                        }
-                        if (int.TryParse(Console.ReadLine(), out int blogNum))
-                        {
-                            foreach (var item in query)
-                            {
-                                if (tempMax < item.BlogId)
-                                {
-                                    tempMax = item.BlogId;
-                                }
-                            }
-                            if (blogNum == 0)
-                            {
-                                foreach (var item in db.Posts.OrderBy(p => p.Title))
-                                {
-                                    Console.WriteLine($"Blog: {item.Blog.Name}\nTitle: {item.Title}\nContent: {item.Content}\n");
-                                }
-                            }
-                            else if ((blogNum < tempMax) && (blogNum > 0))
-                            {
-                                foreach (var item in db.Posts.Where(p => p.BlogId == blogNum).OrderBy(p => p.Title))
-                                {
-                                    Console.WriteLine($"Blog: {item.Blog.Name}\nTitle: {item.Title}\nContent: {item.Content}\n");
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("Invalid Blog Number");
-                                logger.Error("Invalid Blog Number");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Blog is NaN");
-                            logger.Error("Blog is Nan");
-                        }
-                    }
-                } while (answer == "1" || answer == "2" || answer == "3" || answer == "4");
+                    
+                } while (answer == "1" || answer == "2" || answer == "3");
             }
             catch (Exception ex)
             {
